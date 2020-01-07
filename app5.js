7fd6b958af644eb5ffb97e3a014791a3c04330aa
Krisnaxda <krisna.amarta4c@gmail.com>
2020-01-07 12:01:04 +0700

    tambah lanjutan

--- app5.js
+++ app5.js
@@ -2,9 +2,14 @@ new Vue({
   el: '#app',
   data:{
     balik: 'http://127.0.0.1:9000/lanjutan2.html',
-    lanjut: 'http://127.0.0.1:9000/lanjutan4.html'
+    lanjut: 'http://127.0.0.1:9000/lanjutan4.html',
+    output: 'Biru'
   },
   methods:{
+    bacaRefs:function(){
+      console.log(this.$refs.test.innerText);
+      this.output = this.$refs.isian.value;
+    }
 
   },
   computed:{
